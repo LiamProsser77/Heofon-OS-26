2eb445a51278838916e59ccf3216500531373ba3
HeofonGlobalSearch <32lprosser@kimberly.k12.wi.us>
2026-05-15 16:27:12 -0500

    Create wallpaper.ts

---
+++ wallpaper.ts
@@ -0,0 +1,2 @@
+export const DEFAULT_WALLPAPER = 'https://images.fillout.com/orgid-694433/flowpublicid-aiblteb1zy/widgetid-default/i7wbMsdjD116Yz9sGg44zj/pasted-image-1778878377568-dvabifh5.png';
+export const WALLPAPER_KEY = 'heofon-wallpaper';
